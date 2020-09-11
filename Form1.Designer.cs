6fd5bfb87f70a3cb8e1ca5afcad5105acecd2d47
hpz800 <hpz800@DESKTOP-APDTGQ1>
2020-09-11 09:43:00 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace SamplViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            // Disposes and clears entries inside m_featureTypeDictionary
+            resetAllDictionaries();
+
+            // Every FME Objects application MUST at the very least dispose the
+            // session to ensure clean up of all resources. For applications
+            // that require efficient use of memory, Dispose() is available on
+            // all FME Objects interfaces and should be called immediately after
+            // they are no longer needed to immediately clean up resources.
+            if (m_fmeSession != null)
+            {
+                m_fmeSession.Dispose();
+                m_fmeSession = null;
+            }
+            if (m_fmeGeometryTools != null)
+            {
+                m_fmeGeometryTools.Dispose();
+                m_fmeGeometryTools = null;
+            }
+
+            if (m_fmeLogFile != null)
+            {
+                m_fmeLogFile.Dispose();
+                m_fmeLogFile = null;
+            }
+            if (m_fmeDialog != null)
+            {
+                // m_fmeDialog.Dispose();
+                m_fmeDialog = null;
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.featureTypeView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(484, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(39, 21);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.openToolStripMenuItem.Text = "Open...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openOption_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As...";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitOption_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(47, 21);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.aboutToolStripMenuItem.Text = "About...";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutOption_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Location = new System.Drawing.Point(0, 28);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(484, 239);
+            this.tabControl1.TabIndex = 1;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.featureTypeView);
+            this.tabPage1.Controls.Add(this.statusStrip1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(476, 213);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Feature Type";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // featureTypeView
+            // 
+            this.featureTypeView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.featureTypeView.FullRowSelect = true;
+            this.featureTypeView.GridLines = true;
+            this.featureTypeView.HideSelection = false;
+            this.featureTypeView.Location = new System.Drawing.Point(0, 0);
+            this.featureTypeView.MultiSelect = false;
+            this.featureTypeView.Name = "featureTypeView";
+            this.featureTypeView.Size = new System.Drawing.Size(476, 185);
+            this.featureTypeView.TabIndex = 1;
+            this.featureTypeView.UseCompatibleStateImageBehavior = false;
+            this.featureTypeView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Feature Type";
+            this.columnHeader1.Width = 173;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Count";
+            this.columnHeader2.Width = 87;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(3, 188);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(470, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(476, 213);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Geometry";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(476, 213);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Coord Sys";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(476, 213);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Format Info";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(476, 213);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Schema Info";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.ListView featureTypeView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Safe.FMEObjects;
+using System.Collections.Specialized;
+
+namespace SamplViewer
+{
+    public partial class Form1 : Form
+    {
+        private IFMEOSession m_fmeSession = null;
+        private IFMEOGeometryTools m_fmeGeometryTools = null;
+        private IFMEOLogFile m_fmeLogFile = null;
+        private IFMEODialog m_fmeDialog = null;
+        private FMEOFormatInfo m_dataInfo = null;
+        private StringCollection m_createDirectives = null;
+        private const int m_kUpdateInterval = 500;
+        private SortedList<string, IFMEOFeatureVectorOnDisk> m_featureTypeDictionary = null;
+        private const int m_kMaxFeatureInMemory = 500;
+        private enum eTabPanels
+        {
+            FeatureTypeTab = 0,
+            GeometryTab,
+            CoordSysTab,
+            FormatInfoTab,
+            SchemaInfoTab
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            m_fmeSession = FMEObjects.CreateSession();
+            m_fmeSession.Init(null);
+
+            m_fmeGeometryTools = m_fmeSession.GeometryTools();
+
+            m_fmeLogFile = m_fmeSession.LogFile();
+            m_fmeLogFile.SetFileName("logfile.log", false);
+            m_fmeDialog = m_fmeSession.CreateDialogBox();
+            m_fmeDialog.SetParentWindow(this.Handle);
+
+            m_dataInfo = new FMEOFormatInfo();
+            m_createDirectives = new StringCollection();
+
+            m_featureTypeDictionary = new SortedList<string, IFMEOFeatureVectorOnDisk>();
+            // Disable the tab panels
+            tabControl1.Enabled = false;
+
+
+
+        }
+
+
+        private void updateStatusBar(string pText)
+        {
+            toolStripStatusLabel1.Text = pText;
+            statusStrip1.Refresh();
+        }
+        private void exitOption_Click(object sender, System.EventArgs e)
+        {
+            this.Dispose();
+            Application.Exit();
+        }
+
+        private void aboutOption_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                m_fmeDialog.About("Sample Application");
+            }
+            catch (FMEOException ex)
+            {
+                // Log errors to log file
+                m_fmeLogFile.LogMessageString(ex.FmeErrorMessage,
+                 FMEOMessageLevel.Error);
+                m_fmeLogFile.LogMessageString(ex.FmeStackTrace,
+                 FMEOMessageLevel.Error);
+                m_fmeLogFile.LogMessageString(ex.FmeErrorNumber.ToString(),
+                 FMEOMessageLevel.Error);
+
+                // Now exit the application
+                Application.Exit();
+            }
+        }
+        private void openOption_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                if (m_fmeDialog.SourcePrompt("", "", m_dataInfo, m_createDirectives))
+                {
+                    // Disposes and clears entries inside m_featureTypeDictionary
+                    resetAllDictionaries();
+
+                    // Add IFMEOReader code here
+                    // Update status bar
+                    updateStatusBar("Reading from source...");
+
+                    // If we are here, then the user did not hit Cancel. Now let's
+                    // create a reader to read in features. Caching is enabled for
+                    // the reader to improve performance on subsequent re-reads.
+                    IFMEOReader fmeReader = m_fmeSession.CreateReader(m_dataInfo.Format, false,
+                   m_createDirectives);
+                    // Open the reader
+                    StringCollection openParams = new StringCollection();
+                    fmeReader.Open(m_dataInfo.Dataset, openParams);
+
+                    int featureCount = 0;
+                    // Now, read in the data features
+                    readDataFeatures(fmeReader, ref featureCount);
+                    // Update status bar with final feature count
+                    updateStatusBar("Total features: " + featureCount.ToString());
+
+                    // Clean up reader
+                    fmeReader.Close();
+                    fmeReader.Dispose();
+                    fmeReader = null;
+
+                    // Refresh the current tab
+                    refreshCurrentTab();
+
+                    // Enable the tab panels
+                    tabControl1.Enabled = true;
+                }
+            }
+            catch (FMEOException ex)
+            {
+                // Log errors to log file
+                m_fmeLogFile.LogMessageString(ex.FmeErrorMessage, FMEOMessageLevel.Error);
+                m_fmeLogFile.LogMessageString(ex.FmeStackTrace, FMEOMessageLevel.Error);
+                m_fmeLogFile.LogMessageString(ex.FmeErrorNumber.ToString(),
+                FMEOMessageLevel.Error);
+
+                // Now exit the application
+                Application.Exit();
+            }
+        }
+        private void readDataFeatures(IFMEOReader fmeReader, ref int featureCount)
+        {
+            // Initialize counters
+            int numFeatures = 0;
+
+            // Now, read all the different features
+            IFMEOFeature fmeFeature = m_fmeSession.CreateFeature();
+            while (fmeReader.Read(fmeFeature))
+            {
+                // Log the feature to the log file
+                m_fmeLogFile.LogFeature(fmeFeature, FMEOMessageLevel.Inform, -1);
+
+                // Increment feature count
+                numFeatures++;
+
+                // For every feature, we will put it into m_featureTypeDictionary
+                insertIntoFeatureTypeDictionary(fmeFeature);
+
+                // Check if we need to update status bar
+                if ((numFeatures % m_kUpdateInterval) == 0)
+                {
+                    updateStatusBar("Read " + numFeatures.ToString() + " features...");
+                }
+
+
+                // Create a new feature for the next read
+                fmeFeature = m_fmeSession.CreateFeature();
+
+            }
+            fmeFeature.Dispose();
+
+            featureCount = numFeatures;
+        }
+        private void insertIntoFeatureTypeDictionary(IFMEOFeature pFeature)
+        {
+            string currFeatureType = pFeature.FeatureType;
+            // First, we check for where to put the feature inside m_featureTypeDictionary
+            // Check to see if an entry for currFeatureType already exists
+            if (!m_featureTypeDictionary.ContainsKey(currFeatureType))
+            {
+                // We must create a new entry to the m_featureTypeDictionary
+                IFMEOFeatureVectorOnDisk newVectorOnDisk =
+                m_fmeSession.CreateFeatureVectorOnDisk(m_kMaxFeatureInMemory);
+                // Add the new entry to the dictionary
+                m_featureTypeDictionary.Add(currFeatureType, newVectorOnDisk);
+            }
+            // Now, we put the feature into the proper IFMEOFeatureVectorOnDisk
+            IFMEOFeatureVectorOnDisk currVectorOnDisk = m_featureTypeDictionary[currFeatureType];
+            currVectorOnDisk.Append(pFeature);
+        }
+        private void disposeFeatureTypeDictionaryEntries()
+        {
+            IEnumerator<KeyValuePair<string, IFMEOFeatureVectorOnDisk>> iterator =
+            m_featureTypeDictionary.GetEnumerator();
+            // Iterate through each entry m_featureTypeDictionary, and clear each
+            // IFMEOFeatureVectorOnDisk
+            while (iterator.MoveNext())
+            {
+                string currFeatureType = iterator.Current.Key;
+                IFMEOFeatureVectorOnDisk currVectorOnDisk = iterator.Current.Value;
+                // Calling Clear of IFMEOFeatureVectorOnDisk should also dispose each of the
+                // features that it contains
+                currVectorOnDisk.Clear();
+                currVectorOnDisk.Dispose();
+                currVectorOnDisk = null;
+
+            }
+            // Finally, call Clear of m_featureTypeDictionary
+            m_featureTypeDictionary.Clear();
+        }
+        private void resetAllDictionaries()
+        {
+            // Dispose all the features inside of m_featureTypeDictionary
+            disposeFeatureTypeDictionaryEntries();
+        }
+        private void refreshCurrentTab()
+        {
+            eTabPanels selectedPanel = (eTabPanels)tabControl1.SelectedIndex;
+            // Update the corresponding tab
+            switch (selectedPanel)
+            {
+                case eTabPanels.FeatureTypeTab:
+                    updateFeatureTypeTab();
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void tabControl1_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            refreshCurrentTab();
+        }
+        private void updateFeatureTypeTab()
+        {
+            // Clear the current entries in the featureTypeView
+            featureTypeView.Items.Clear();
+            IEnumerator<KeyValuePair<string, IFMEOFeatureVectorOnDisk>> iterator =
+             m_featureTypeDictionary.GetEnumerator();
+            while (iterator.MoveNext())
+            {
+                string currFeatureType = iterator.Current.Key;
+                IFMEOFeatureVectorOnDisk currVectorOnDisk =
+                m_featureTypeDictionary[currFeatureType];
+                // Create a new subItem list - it will be a row of data containing
+                // the feature type and its associated count
+                string[] subItemList = { currFeatureType, currVectorOnDisk.Count.ToString() };
+                ListViewItem newItem = new ListViewItem(subItemList, -1);
+                // Add the item to featureTypeView
+                featureTypeView.Items.Add(newItem);
+            }
+        }
+
+
+    }
+
+}
